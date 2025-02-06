7b03b2e1a2937feec35d3f2593a75230e60ccc5b
naufalparamarafif <profesoraiueo@gmail.com>
2025-02-06 22:31:27 +0700

    Add milestone resource

---
+++ app/Filament/Resources/MilestoneResource.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\MilestoneResource\Pages;
+use App\Filament\Resources\MilestoneResource\RelationManagers;
+use App\Models\Milestone;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class MilestoneResource extends Resource
+{
+    protected static ?string $model = Milestone::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Card::make()
+                ->schema([
+                    Forms\Components\TextInput::make('year')
+                        ->numeric()
+                        ->minValue(1950)
+                        ->maxValue(2050),
+                    Forms\Components\TextArea::make('caption')
+                        ->required()
+                        ->maxLength(200),
+                ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('caption')->searchable(),
+                Tables\Columns\TextColumn::make('year')->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListMilestones::route('/'),
+            'create' => Pages\CreateMilestone::route('/create'),
+            'edit' => Pages\EditMilestone::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/MilestoneResource/Pages/CreateMilestone.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Filament\Resources\MilestoneResource\Pages;
+
+use App\Filament\Resources\MilestoneResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateMilestone extends CreateRecord
+{
+    protected static string $resource = MilestoneResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
