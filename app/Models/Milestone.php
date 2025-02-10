2e3b69f21f3c00eb112d93b2c7e625e5ab8e4b42
Fadlan966 <fgsn.iu@gmail.com>
2025-02-10 18:12:26 +0700

    penambahan isian milestone

--- app/Models/Milestone.php
+++ app/Models/Milestone.php
@@ -8,4 +8,30 @@ use Illuminate\Database\Eloquent\Model;
 class Milestone extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'milestones';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'caption',
+        'year',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'year' => 'integer',
+    ];
 }
